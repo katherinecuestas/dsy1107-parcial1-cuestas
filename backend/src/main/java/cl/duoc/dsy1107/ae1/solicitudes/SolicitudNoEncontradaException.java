16b2f29e21a562923c12647be2c04a5175614cfb
Katherine Cuestas <ka.cuestas@duocuc.cl>
2026-09-14 12:51:13 -0300

    Agrega @ResponseStatus 404 a SolicitudNoEncontradaException para que una solicitud cancelada de 404, no 200

--- backend/src/main/java/cl/duoc/dsy1107/ae1/solicitudes/SolicitudNoEncontradaException.java
+++ backend/src/main/java/cl/duoc/dsy1107/ae1/solicitudes/SolicitudNoEncontradaException.java
@@ -1,5 +1,9 @@
 package cl.duoc.dsy1107.ae1.solicitudes;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class SolicitudNoEncontradaException extends RuntimeException {
     public SolicitudNoEncontradaException(Long id) {
         super("No existe la solicitud con id " + id);
